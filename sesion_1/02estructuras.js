4d2738336667a59d0799dcb0d1528cd9c00a41c9
Jhoana Vargas <Jhoana vargas>
2026-07-02 21:41:26 -0500

    Actualización del proyecto

--- sesion_1/02estructuras.js
+++ sesion_1/02estructuras.js
@@ -1,70 +1,68 @@
 // ciclos for , wile  , do-wile
 
 
-//1...estructura for :cuando se sabe cuantas veses repetir el ciclo
-for (let i =0; i < 5; i++){
-    console.log("interaccion de numeros: i"   );
-}
+console.log()
 
-//wile
+
+
+
+//whilw
 let intentos = 0;
-const max_intentos = 3;
+const MAX_INTENTOS = 3;
 
-wile (intentos < max_intentos) 
-    console.log('intentos', intentos+1, 'de', max_intentos);
-    intentos++;
-   
+while (intentos < MAX_INTENTOS) {
+    console.log('intento', intentos+1, 'de',MAX_INTENTOS);
+    intentos ++;
+}
 
 // break y continue
-for (let i = 0; i < 10; i++){
-    if (i === 4 )continue;
-    if (i === 7)break;
-    console.log(1);
+for (let i = 0; i < 10; i++) {
+    if(i === 4) continue;
+    if (i===7) break;
+    console.log(i);
 }
 
-//funciones
+// funciones
 //funcion clasica
 function saludar(nombre){
-    return 'hola tu nombre es, $(nombre)';
+    return 'Hola tu nombre es, ${nombre}';
 }
 
-console.log(saludar("sofia maria"));
+console.log(saludar("Ana Maria"));
 
-
-//2...funcion anonima asignada o variable, no tiene nombre propio, porque se guarda en una variable
-let calculararea = function(base, altura){
+//2. funcion anonima asignada a variable no tiene nombre propio, se guarda en una variable
+let calcularArea = function (base, altura){
     return base*altura;
 }
-console.log(calculararea(6,8));
+console.log(calcularArea(6,8));
 
+//3.funciones arrow
+let sumar = (vali, val2)=> vali+val2;
+console.log(sumar(12000,45000));
 
-//3... funcion arrow
-let sumar = (val1, val2) => val1+val2;
-console.log(sumar(5,3));
-
-//4...parametros
-function crearproducto(nombre, precio, stock= 5){
-    return { nombre, precio, stock};
+//4. parametros
+function crearProducto(nombre, precio, stock= 5) {
+    return { nombre,precio, stock};
 }
-console.log(crearproducto("silla", 15000));
-console.log(crearproducto("silla", 15000, 50));
+console.log(crearProducto("silla", 75000));
+console.log(crearProducto("silla", 75000,50));
 
+//alcance de una variable
+const appNombre= "StoreApp"; // variable accesible desde cualquier parte del archivo
 
-//alcanse de una variable
-const appnombre  = "storeapp"; //variable accesible desde cualquier perte del archivo}
+function pagoAlquiler() {
+    let totalFactura = 3000000; //variable accesible dentro de la factura
+}
+// console.log('proceso de pago: $ (total factura)');
 
-function pagoalquiler () {
-    let totalfactura = 3000000; } // variable accesible dentro de la funcion
- //console.log('proceso de pago:${totalfactura}'):
+//5. callback una funcion puede recibir como parametro otra funcion
+function procesoOperacion(a,b, operacion) {
+    let resultado= operacion (a,b)
+    return 'resulltado:$(resultado)';
+}
+let multiplicar = (x,y) => x*y;
+let restar= (x,y) => x-y;
 
+console.log(procesarOperacion(10,5, multiplicar));
+console.log(procesarOperacion(10,5, restar));
 
- //5...calback una funcion puede recibir como parametro ota funcion
- function procesaroperacion(a,b, Operacion){
-     let resultado = Operacion(a,b)
-     return 'resultado:$(resultado)';
- }
- let multiplicar = (x,y) => x*y;
- let restar = (x,y) => x-y;
-
- console.log(procesaroperacion(10,5,multiplicar));
- console.log(procesaroperacion(10,5,restar));
